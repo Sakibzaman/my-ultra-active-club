ca5ebc165b3a874f960778337c0e727f11991680
Sakib Zaman <sakibzaman19@gmail.com>
2022-09-29 18:13:49 +0600

    activity completed button added

--- src/components/Home/VerticalBar/ExerciseDetails.js
+++ src/components/Home/VerticalBar/ExerciseDetails.js
@@ -11,6 +11,9 @@ const ExerciseDetails = () => {
         <div className="mt-5 p-4 text-xl rounded-2xl bg-gray-100">
           <h1>Break Time</h1>
         </div>
+        <button className="bg-indigo-500 rounded-xl text-white w-full my-3 py-2">
+          Activity Completed
+        </button>
       </div>
     </div>
   );
